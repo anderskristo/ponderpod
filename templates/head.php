6c7edab201f13984eb5587c877d5e03ffb00650b
Anders Kristoffersson <anderskristo@gmail.com>
2015-05-19 20:56:52 +0200

    added google font

--- templates/head.php
+++ templates/head.php
@@ -4,5 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Google Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,700,300' rel='stylesheet' type='text/css'>
     <?php wp_head(); ?>
   </head>
