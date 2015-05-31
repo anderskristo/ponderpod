a4cc756682c6992134466521768de48671bfc973
Anders Kristoffersson <anderskristo@gmail.com>
2015-05-31 20:42:50 +0200

    added favicon

--- templates/head.php
+++ templates/head.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/ico" href="<?php bloginfo('template_directory') ?>/favicon.ico" />
 
     <!-- Google Fonts -->
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,700,300' rel='stylesheet' type='text/css'>
